5a0fea2c73666de539b74564fd3652f3771bd6e8
Dennis <dennisndegwa001@gmail.com>
2023-08-03 11:54:46 +0300

    Change the way we describe the distFolder in the server

--- server.ts
+++ server.ts
@@ -10,9 +10,10 @@ import { AppServerModule } from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/DpoClient/browser');
+  // const distFolder = join(process.cwd(), 'dist/DpoClient/browser');
+  // const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const distFolder = join(__dirname, "../browser");
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
-
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule
